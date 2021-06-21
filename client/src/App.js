57eeab8e33f8de52ab6fc5abae1da87dfbf0a495
Daniel Kim <kkdh1109@gmail.com>
2021-06-20 22:22:15 -0400

    create chat and sidebar comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Sidebar from './component/Sidebar/Sidebar';
+import Chat from './component/Chat/Chat';
 
 function App() {
   return (
     <div className="app">
-
+      <div className="app__body">
+        <Sidebar />
+        <Chat />
+      </div>
+      
     </div>
   );
 }
---
+++ client/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAL-vJIOUs3wUvlg54i-s0NMYR9AeZtqBw",
+    authDomain: "whatsapp-d892b.firebaseapp.com",
+    projectId: "whatsapp-d892b",
+    storageBucket: "whatsapp-d892b.appspot.com",
+    messagingSenderId: "718245619305",
+    appId: "1:718245619305:web:25f1414d4fda65249aa4f8",
+    measurementId: "G-2HK16YTLX2"
+  };
