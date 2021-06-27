76721814d2365ab448fe3780c180f67cdc21b73f
Daniel Kim <kkdh1109@gmail.com>
2021-06-27 16:03:39 -0400

    add touchable icons and avatar

--- client/src/component/Sidebar/Sidebar.js
+++ client/src/component/Sidebar/Sidebar.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './Sidebar.css';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { Avatar, IconButton } from '@material-ui/core';
 
 function Sidebar() {
     return (
         <div className='sidebar'>
             <div className="sidebar__header">
+                <Avatar src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvbHENQ2AcJp0uUW7mXUDwBW1v-XWru9xhPQ&usqp=CAU' />
                 <div className="sidebar__headerRight">
-                    <DonutLargeIcon />
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
                 </div>
             </div>
         </div>
